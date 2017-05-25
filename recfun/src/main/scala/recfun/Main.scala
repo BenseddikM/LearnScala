7a085c217cbffab2644a36359f4134a1d2171376
BenseddikM <benseddikmohammed@gmail.com>
2017-05-25 17:13:45 +0200

    WeekOne Exams Finished and succefull

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -8,20 +8,59 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+
+    println("************************")
+    println("Parentheses Balancing")
+    var testStr = "HEllo (( )) my (freind).".toList
+    println(testStr.toString())
+    println(balance(testStr))
+    testStr = "HEllo (( ) my (freind).".toList
+    println(testStr.toString())
+    println(balance(testStr))
+    testStr = ")HEllo (( ) my (freind).".toList
+    println(testStr.toString())
+    println(balance(testStr))
+
+    println("************************")
+    println("Counting Change")
+    println(countChange(4, List(1, 2)))
+
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = ???
-  
-  /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || r == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
+
+  /**
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = {
+    def checkBanlanced(chars: List[Char], chk: Int): Boolean = {
+      if (chars.isEmpty) chk == 0
+      else if (chars.head == '(') checkBanlanced(chars.tail, chk + 1)
+      else if (chars.head == ')') chk > 0 && checkBanlanced(chars.tail, chk - 1)
+      else checkBanlanced(chars.tail, chk)
+    }
+
+    checkBanlanced(chars, 0)
+  }
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = {
+
+    def loop(money: Int, coins: List[Int]): Int = {
+      if (money < 0 || coins.isEmpty) 0
+      else if (money == 0) 1
+      else loop(money, coins.tail) + loop(money - coins.head, coins)
+    }
+
+    loop(money, coins)
+  }
+
+}
